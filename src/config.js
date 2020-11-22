f4d6e88b1aa325647d02b8e7b880685e55851e42
Stewart Jester <jesterstewart@gmail.com>
2020-11-21 17:18:46 -0800

    changed endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     // API_ENDPOINT: `http://localhost:8000/api/`, 
-    API_ENDPOINT: `https://intense-caverns-53160.herokuapp.com/api/`,
+    API_ENDPOINT: `https://git.heroku.com/fierce-journey-48260.git/api/`,
     TOKEN_KEY: 'easycal-app-auth-token'
   }
